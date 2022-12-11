dc5e9d8334fa67e38ad3825a8969468ba6c242f8
Obiri Job <obirijob20@gmail.com>
2022-12-11 18:33:08 +0300

    Added Search and resolved svg error

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,22 +1,32 @@
 /** @format */
 
-import { View, Text, Dimensions } from 'react-native'
+import { Dimensions } from 'react-native'
 import React from 'react'
 import colors from '../assets/Colors'
+import { SafeAreaView } from 'moti'
+import { BlurView } from 'expo-blur'
+import CustomText from './CustomText'
 
 const Search = () => {
   const { width, height } = Dimensions.get('screen')
   return (
-    <View
+    <BlurView
       style={{
         position: 'absolute',
         height,
         width,
-        backgroundColor: colors.WHITE,
+        backgroundColor: colors.WHITE + '55',
+        paddingHorizontal: 30,
+        paddingTop: 50,
       }}
     >
-      <Text>Search</Text>
-    </View>
+      <SafeAreaView>
+        <CustomText
+          content={'Search'}
+          style={{ fontFamily: 'db', fontSize: 50, color: colors.GREEN }}
+        />
+      </SafeAreaView>
+    </BlurView>
   )
 }
 
